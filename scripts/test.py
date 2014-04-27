575e8e042b166ef1385ed5d0b3b5a59d7c203ce1
Sam King <sam@samking.org>
2014-04-27 14:50:11 -0700

    Made unit tests work outside of the project dir.

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Runs all unit tests."""
 
+import os
 import subprocess
 
 # To support running this file from the root, pylint: disable=F0401
@@ -12,6 +13,8 @@ def run_tests():
     """Runs unit tests using nose and the NoseGAE plugin."""
     app_engine_dir = common.get_app_engine_dir()
     project_dir = common.get_project_dir()
+    # NoseGAE wants us to be in the project directory.
+    os.chdir(project_dir)
     subprocess.call(
         ['nosetests', '--with-gae', '--without-sandbox', '--gae-lib-root',
          app_engine_dir, project_dir])
