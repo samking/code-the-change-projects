c40f5ec0c9625125f5dbb147ae7d1a7ebbd1812f
Sam King <sam@samking.org>
2014-04-27 17:32:42 -0700

    Added support for owners in project test helper.

--- testing/model_helpers.py
+++ testing/model_helpers.py
@@ -1,14 +1,17 @@
 """Helpers for testing models."""
 
 from models import project
+from models import user
 
 
 def create_project(
         title='title', description='description', lead='lead',
-        tech_objectives='tech_objectives', github='github'):
+        tech_objectives='tech_objectives', github='github', owner_key=None):
     """Returns a new datastore-backed project."""
+    if owner_key is None:
+        owner_key = user.User().put()
     new_project = project.Project(
         title=title, description=description, lead=lead,
-        tech_objectives=tech_objectives, github=github)
+        tech_objectives=tech_objectives, github=github, owner_key=owner_key)
     new_project.put()
     return new_project
